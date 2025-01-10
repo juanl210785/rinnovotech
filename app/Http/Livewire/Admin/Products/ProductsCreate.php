217d19d5b8f8572f12a995325fbace755dbd8928
Juan Luis <juanherrera075@gmail.com>
2025-01-09 23:38:02 -0400

    ProductCreate-Fail

--- app/Http/Livewire/Admin/Products/ProductsCreate.php
+++ app/Http/Livewire/Admin/Products/ProductsCreate.php
@@ -88,10 +88,10 @@ class ProductsCreate extends Component
         ]);
 
         session()->flash('notification', [
-            'clase' => 'text-dander',
-            'lucide' => 'x-circle',
-            'title' => 'Error',
-            'message' => '¡Producto no ha sido creado!'
+            'clase' => 'text-success',
+            'lucide' => 'check-circle',
+            'title' => 'Exito',
+            'message' => '¡Producto ha sido creado!'
         ]);
 
         return redirect()->route('admin.products.create');
@@ -252,6 +252,6 @@ class ProductsCreate extends Component
 
     public function render()
     {
-        return view('livewire.admin.products.products-create')->with('flashMessage', session('message'));
+        return view('livewire.admin.products.products-create');
     }
 }
