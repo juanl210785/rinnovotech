92521b830ec8597a29338d7f9c6f53047c26e08a
juanl210785 <juanherrera075@gmail.com>
2025-01-15 16:51:13 -0400

    Option Create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,18 +4,19 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+
     public function run()
     {
+        Storage::deleteDirectory('products');
+        Storage::makeDirectory('products');
+
         $this->call(UserSeeder::class);
         $this->call(FamilySeeder::class);
+        $this->call(OptionSeeder::class);
 
         Product::factory(50)->create();
     }
